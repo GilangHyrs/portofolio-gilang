b670c8f2f5fec0a3df98aa6495dd5c0a10d94554
GilangHyrs <gilanghadyr@gmail.com>
2024-04-07 02:24:14 +0700

    first commit

---
+++ src/components/Skills.vue
@@ -0,0 +1,242 @@
+<script setup>
+import { computed, defineComponent, reactive, ref } from 'vue';
+import { useDisplay } from 'vuetify';
+
+const { xs, sm, md, lg, xlAndUp } = useDisplay();
+
+defineComponent({
+    name: 'Skills',
+});
+
+const skills = reactive({
+    options: [
+        {
+            id: 1,
+            name: 'Networking',
+        },
+        {
+            id: 2,
+            name: 'Microsoft Office',
+        },
+        {
+            id: 3,
+            name: 'Figma',
+        },
+        {
+            id: 4,
+            name: 'Canva',
+        },
+        {
+            id: 5,
+            name: 'Other',
+        },
+        {
+            id: 6,
+            name: 'Other',
+        },
+    ],
+    items: [
+        {
+            id: 1,
+            option: 1,
+            name: 'HTML',
+            experience: '3 months',
+        },
+        {
+            id: 2,
+            option: 1,
+            name: 'other',
+            experience: '0',
+        },
+        {
+            id: 3,
+            option: 1,
+            name: 'other',
+            experience: '0',
+        },
+        {
+            id: 4,
+            option: 1,
+            name: 'other',
+            experience: '0',
+        },
+        {
+            id: 5,
+            option: 2,
+            name: 'Vue.js',
+            experience: '1 months',
+        },
+        {
+            id: 6,
+            option: 2,
+            name: 'other',
+            experience: '0',
+        },
+        {
+            id: 7,
+            option: 3,
+            name: 'other',
+            experience: '0',
+        },
+        {
+            id: 8,
+            option: 3,
+            name: 'Bootstrap',
+            experience: '1 months',
+        },
+        {
+            id: 9,
+            option: 3,
+            name: 'other',
+            experience: '0',
+        },
+        {
+            id: 10,
+            option: 4,
+            name: 'MySQL',
+            experience: '3 months',
+        },
+        {
+            id: 11,
+            option: 4,
+            name: 'Other',
+            experience: '0',
+        },
+        {
+            id: 12,
+            option: 4,
+            name: 'other',
+            experience: '0',
+        },
+        {
+            id: 13,
+            option: 4,
+            name: 'SQL Server',
+            experience: '1 months',
+        },
+        {
+            id: 14,
+            option: 4,
+            name: 'other',
+            experience: '0',
+        },
+        {
+            id: 15,
+            option: 4,
+            name: 'other',
+            experience: '0',
+        },
+        {
+            id: 16,
+            option: 5,
+            name: 'other',
+            experience: '0',
+        },
+        {
+            id: 17,
+            option: 5,
+            name: 'other',
+            experience: '0',
+        },
+        {
+            id: 18,
+            option: 5,
+            name: 'other',
+            experience: '0',
+        },
+        {
+            id: 19,
+            option: 5,
+            name: 'other',
+            experience: '0',
+        },
+        {
+            id: 20,
+            option: 5,
+            name: 'other',
+            experience: '0',
+        },
+        {
+            id: 21,
+            option: 6,
+            name: 'Figma',
+            experience: '2 months',
+        },
+        {
+            id: 22,
+            option: 6,
+            name: 'Photoshop',
+            experience: '1 months',
+        },
+        {
+            id: 23,
+            option: 6,
+            name: 'other',
+            experience: '0',
+        },
+        {
+            id: 24,
+            option: 6,
+            name: 'other',
+            experience: '0',
+        },
+    ],
+});
+
+const activeOption = ref(null);
+
+const skillItems = computed(() =>
+    activeOption.value != null ? skills.items.filter((item) => item.option === activeOption.value) : skills.items,
+);
+
+const toggleOption = (option) => (activeOption.value = activeOption.value === option.id ? null : option.id);
+</script>
+<template>
+    <div class="technical-skills">
+        <h5 class="text-h5 text-uppercase text-center text-deep-purple-accent-1">Technical skills</h5>
+        <v-slide-group show-arrows center-active class="mt-3">
+            <v-slide-group-item v-for="option in skills.options" :key="option.id" v-slot="{ isSelected, toggle }">
+                <v-btn
+                    class="ma-2"
+                    rounded
+                    :color="option.id === activeOption ? 'deep-purple-accent-1' : undefined"
+                    @click="toggleOption(option)"
+                >
+                    {{ option.name }}
+                </v-btn>
+            </v-slide-group-item>
+        </v-slide-group>
+        <v-item-group selected-class="bg-deep-purple-accent-1" class="technical-skills__row">
+            <v-row>
+                <v-col
+                    v-for="skill in skillItems"
+                    :key="skill.id"
+                    :cols="xs ? 12 : sm ? 6 : md || lg ? 4 : xlAndUp ? 3 : undefined"
+                >
+                    <v-item v-slot="{ isSelected, selectedClass, toggle }">
+                        <v-card :class="['technical-skills__item', selectedClass]" @click="toggle">
+                            <div class="text-h5 text-center">{{ isSelected ? skill.experience : skill.name }}</div>
+                        </v-card>
+                    </v-item>
+                </v-col>
+            </v-row>
+        </v-item-group>
+    </div>
+</template>
+
+<style scoped>
+.technical-skills__row {
+    display: flex;
+    margin-top: 24px;
+}
+
+.technical-skills__item {
+    display: flex;
+    flex: 0 1 25%;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    height: 80px;
+    user-select: none;
+}
+</style>
---
+++ src/components/Timelines.vue
@@ -0,0 +1,84 @@
+<script setup>
+import { defineComponent, reactive } from 'vue';
+
+defineComponent({
+    name: 'Timelines',
+});
+
+const timelines = reactive([
+    {
+        id: 0,
+        icon: 'mdi-book-education',
+        color: 'cyan lighten-1',
+        title: 'University Islam Riau',
+        position: 'Teknik Informatika S1',
+        period: '2022',
+        description: '',
+    },
+    {
+        id: 1,
+        icon: 'mdi-book-education',
+        color: 'amber darken-1',
+        title: 'SMAN 1 UKUI ',
+        position: 'SISWA',
+        period: '2019-2022',
+        description: '',
+    },
+    {
+        id: 2,
+        icon: 'mdi-book-education',
+        color: 'blue-darken-3',
+        title: 'SMPN 1 UKUI ',
+        position: 'SISWA',
+        period: '2016-2019',
+        description: '',
+    },
+    {
+        id: 3,
+        icon: 'mdi-book-education',
+        color: 'blue-grey',
+        title: 'SDN 001 UKUI',
+        position: 'SISWA',
+        period: '2010 — 2016',
+        description: '',
+    },
+    {
+        id: 4,
+        icon: 'mdi-briefcase-variant',
+        color: 'deep-orange-lighten-1',
+        title: 'Anggota Osis SMAN 1 UKUI',
+        position: 'Seksi bidang prestasi akademik, seni, dan/atau olahraga sesuai bakat dan minat',
+        period: '2020 - 2022',
+        description: 'olahraga (Bola kaki,Bola Volly)',
+    },
+]);
+</script>
+
+<template>
+    <div class="timeline mt-5">
+        <h5 class="text-h5 text-uppercase text-center text-deep-purple-accent-1">Education & work</h5>
+        <v-timeline class="mt-5">
+            <v-timeline-item
+                v-for="timeline in timelines"
+                :key="timeline.id"
+                :dot-color="timeline.color"
+                :icon="timeline.icon"
+                size="large"
+            >
+                <template v-slot:opposite>
+                    <div
+                        :class="`pt-1 headline font-weight-bold text-button text-${timeline.color}`"
+                        v-text="timeline.period"
+                    ></div>
+                </template>
+                <div>
+                    <h2 :class="`mt-n1 headline font-weight-light mb-4 text-${timeline.color}`">
+                        {{ timeline.title }}
+                    </h2>
+                    <h5 :class="`text-button text-${timeline.color}`">{{ timeline.position }}</h5>
+                    <span class="text-subtitle-2">{{ timeline.description }}</span>
+                </div>
+            </v-timeline-item>
+        </v-timeline>
+    </div>
+</template>
